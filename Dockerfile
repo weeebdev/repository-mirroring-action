50ebd56bc0ef153b2864d9b04ac88804c13f2cd6
Adil Akhmetov <48881444+weeebdev@users.noreply.github.com>
2022-04-25 23:24:39 +0600

    remove pre-push hook

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,7 @@
 FROM alpine:3.10
 
 RUN apk update && apk upgrade && \
-    apk add --no-cache git openssh git-lfs
-
-RUN git lfs install
+    apk add --no-cache git openssh
 
 COPY mirror.sh /mirror.sh
 COPY setup-ssh.sh /setup-ssh.sh
--- mirror.sh
+++ mirror.sh
@@ -3,8 +3,8 @@ set -eu
 
 /setup-ssh.sh
 
+rm -rf .git/hooks/pre-push
+
 export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
 git remote add mirror "$INPUT_TARGET_REPO_URL"
-git config lfs.allowincompletepush true
-git lfs push --all mirror "refs/remotes/origin/*:refs/heads/*"
 git push --tags --force --prune mirror "refs/remotes/origin/*:refs/heads/*"
