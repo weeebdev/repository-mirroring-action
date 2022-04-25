9acdae785f8416e4a44914bcc4ec8bb08b8d965d
Adil Akhmetov <48881444+weeebdev@users.noreply.github.com>
2022-04-25 23:34:54 +0600

    disable lfs

--- mirror.sh
+++ mirror.sh
@@ -6,5 +6,7 @@ set -eu
 rm -rf .git/hooks/pre-push
 
 export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
+git config --global filter.lfs.smudge "git-lfs smudge --skip -- %f"
+git config --global filter.lfs.process "git-lfs filter-process --skip"
 git remote add mirror "$INPUT_TARGET_REPO_URL"
 git push --tags --force --prune mirror "refs/remotes/origin/*:refs/heads/*"
