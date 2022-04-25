f883c033359a54dc6b2f12ecb291a83d8621fc9d
Adil Akhmetov <48881444+weeebdev@users.noreply.github.com>
2022-04-25 23:20:46 +0600

    Update mirror.sh

--- mirror.sh
+++ mirror.sh
@@ -6,5 +6,5 @@ set -eu
 export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
 git remote add mirror "$INPUT_TARGET_REPO_URL"
 git config lfs.allowincompletepush true
-git lfs push --all
+git lfs push --all mirror "refs/remotes/origin/*:refs/heads/*"
 git push --tags --force --prune mirror "refs/remotes/origin/*:refs/heads/*"
